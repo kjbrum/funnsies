335d5d870c3945525fa3851db28032bc131b792d
Kyle Brumm <kjbrumm@gmail.com>
2016-06-16 06:18:19 -0500

    Add mobile menu js toggle

--- js/src/main.js
+++ js/src/main.js
@@ -1,3 +1,12 @@
 (function($) {
-    // Main JS functionality goes here
+    $('.js-toggle-menu').click(function() {
+        $('.nav--mobile').toggleClass('open');
+
+        console.log($(this).text());
+        if ($(this).text() == 'Menu') {
+            $(this).text('Close');
+        } else {
+            $(this).text('Menu');
+        }
+    });
 })(jQuery);
