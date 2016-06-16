7daa2edb8fbdad6b475dab1e91a8ef9d8c4abbd6
Kyle Brumm <kjbrumm@gmail.com>
2016-06-16 06:44:05 -0500

    Finish building the mobile menu

--- js/src/main.js
+++ js/src/main.js
@@ -1,12 +1,28 @@
 (function($) {
+    // Handle nav menu toggling
     $('.js-toggle-menu').click(function() {
         $('.nav--mobile').toggleClass('open');
 
-        console.log($(this).text());
+        // Change the button text
         if ($(this).text() == 'Menu') {
             $(this).text('Close');
         } else {
             $(this).text('Menu');
+        }
+    });
+
+    // Handle child menu toggling
+    $('.has-child-menu').click(function(e) {
+        e.preventDefault();
+
+        if ($(this).next('.child-menu').hasClass('open')) {
+            $(this).next('.child-menu').removeClass('open');
+        } else {
+            // Hide all child menus
+            $('.child-menu').removeClass('open');
+
+            // Show the new child menu
+            $(this).next('.child-menu').toggleClass('open');
         }
     });
 })(jQuery);
